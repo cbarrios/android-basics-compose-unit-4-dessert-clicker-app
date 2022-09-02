6c900e7038241a02f52f73a8500898bee8193730
cbarrios <lalobarrios16@yahoo.com>
2022-09-01 22:59:26 -0400

    master: (practice) Add a ViewModel and put logic away from the Activity

---
+++ app/src/main/java/com/example/dessertclicker/ui/main/MainViewModel.kt
@@ -0,0 +1,47 @@
+package com.example.dessertclicker.ui.main
+
+import androidx.lifecycle.ViewModel
+import com.example.dessertclicker.data.Datasource.dessertList
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class MainViewModel : ViewModel() {
+
+    private val _uiState = MutableStateFlow(MainUiState())
+    val uiState: StateFlow<MainUiState> = _uiState.asStateFlow()
+
+    fun onDessertClicked() {
+        _uiState.update { uiState ->
+            val dessertsSold = uiState.dessertsSold + 1
+            val nextDessertIndex = determineDessertIndex(dessertsSold)
+            uiState.copy(
+                currentDessertIndex = nextDessertIndex,
+                revenue = uiState.revenue + uiState.currentDessertPrice,
+                dessertsSold = dessertsSold,
+                currentDessertImageId = dessertList[nextDessertIndex].imageId,
+                currentDessertPrice = dessertList[nextDessertIndex].price
+            )
+        }
+    }
+
+    /**
+     * Determine which dessert to show.
+     */
+    private fun determineDessertIndex(dessertsSold: Int): Int {
+        var dessertIndex = 0
+        for (index in dessertList.indices) {
+            if (dessertsSold >= dessertList[index].startProductionAmount) {
+                dessertIndex = index
+            } else {
+                // The list of desserts is sorted by startProductionAmount. As you sell more
+                // desserts, you'll start producing more expensive desserts as determined by
+                // startProductionAmount. We know to break as soon as we see a dessert who's
+                // "startProductionAmount" is greater than the amount sold.
+                break
+            }
+        }
+        return dessertIndex
+    }
+}
